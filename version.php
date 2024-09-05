0f8992305e32d890762c8ffa2a61527419265780
Nigel Daley <nigel.daley@cosector.com>
2024-09-05 13:21:49 +0100

    updated plugin maturity and version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_rollover_wizard';
-$plugin->release = '0.1.0';
+$plugin->release = '1.0';
 $plugin->version = 2024090300;
 $plugin->requires = 2021051700;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
